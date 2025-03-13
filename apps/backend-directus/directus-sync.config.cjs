f860737e6ac8120dd833029e9af5fec4d859ef94
Kimmo Salmela <kimmo.salmela@reun.eu>
2025-03-13 21:27:01 +0200

    Use token instead of user/pw for Directus Sync

--- apps/backend-directus/directus-sync.config.cjs
+++ apps/backend-directus/directus-sync.config.cjs
@@ -1,8 +1,6 @@
 module.exports = {
   directusUrl: "http://0.0.0.0:8055",
-  // directusToken: "my-directus-token",
-  directusEmail: "admin@example.com",
-  directusPassword: "admin123",
+  directusToken: process.env.ADMIN_TOKEN,
   // Don't include specs, since we use GraphQL introspection and the backend
   // needs to be up anyway for the frontend to build.
   specs: false,
