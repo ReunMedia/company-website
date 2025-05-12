e9a6c6363aebddd987e826da66138fb10b56f6ef
Kimmo Salmela <kimmo.salmela@reun.eu>
2025-05-12 17:39:03 +0300

    Improve OG image template layout and add OG preview route

--- apps/frontend-astro/src/ogImages/dynamicPageOgTemplate.ts
+++ apps/frontend-astro/src/ogImages/dynamicPageOgTemplate.ts
@@ -1,10 +1,10 @@
 import { html } from "@reunmedia/astro-og-images";
 import bgImage from "../assets/og-image-bg-01.jpg?url&inline";
-import bgImageMeta from "../assets/og-image-bg-01.jpg";
 import logomarkSvg from "../assets/reun-logomark-01.svg?url&inline";
 import logomarkSvgMeta from "../assets/reun-logomark-01.svg";
 import colorsCss from "../styles/colors.css?inline";
 import { cssColorsToJs } from "../utils/cssColorsToJs";
+import type { Compiler } from "@reunmedia/astro-og-images";
 
 interface Options {
   title: string;
@@ -13,8 +13,19 @@ interface Options {
 
 const colors = cssColorsToJs(colorsCss);
 
-export default function getTemplate({ title, description }: Options) {
-  return html`
+export default function getTemplate({
+  title,
+  description,
+}: Options): ReturnType<typeof html>;
+export default function getTemplate<T>(
+  { title, description }: Options,
+  compiler: Compiler<T>,
+): T;
+export default function getTemplate<T>(
+  { title, description }: Options,
+  compiler?: Compiler<T>,
+) {
+  return (compiler || html)/* html */ `
     <div
       style=${{
         position: "relative",
@@ -28,75 +39,60 @@ export default function getTemplate({ title, description }: Options) {
         backgroundImage: `url(${bgImage})`,
       }}
     >
-      <img
-        width=${bgImageMeta.width}
-        height=${bgImageMeta.height}
-        style=${{
-          position: "absolute",
-          /*
-            You must specify position here to make html preview and satori
-            consistent. use 0 for both values and translate to move. using
-            percentages in position is inconsistent between html and rendered
-            satori image.
-          */
-          left: 0,
-          top: 0,
-          // transform: "translate(57%, -43%)",
-          display: "none",
-        }}
-        src=${bgImage}
-      />
-
       <div
         style=${{
           display: "flex",
           position: "relative",
           width: "100%",
-          gap: "4.5em",
+          flexDirection: "column",
         }}
-      >
+        >
         <div
-          style=${{
-            display: "flex",
-            flex: 1,
-            width: "100%",
-            flexDirection: "column",
-          }}
+        style=${{
+          display: "flex",
+          flex: 1,
+          width: "100%",
+          gap: "4em",
+        }}
         >
           <h2
             style=${{
               fontFamily: "Poppins",
               fontWeight: 400,
-              fontSize: "5em",
+              fontSize: "4.5em",
               margin: 0,
               color: colors["--color-sky-500"],
               flex: 1,
               display: "flex",
               alignItems: "center",
+              lineHeight: "1em",
+              wordBreak: "break-word",
             }}
           >
             ${title}
           </h2>
+            <img
+              width=${logomarkSvgMeta.width / 2}
+              height=${logomarkSvgMeta.height / 2}
+              style=${{ objectFit: "contain" }}
+              src=${logomarkSvg}
+            />
+
+        </div>
+        <div style=${{ display: "flex", flex: 1, alignItems: "center" }}>
           <p
             style=${{
               margin: 0,
               fontFamily: "Inter",
               fontWeight: 500,
-              fontSize: "3em",
+              fontSize: "2.5em",
               flex: 1,
               color: colors["--color-slate-900"],
+              wordBreak: "break-word",
             }}
           >
             ${description}
           </p>
-        </div>
-        <div style=${{ display: "flex" }}>
-          <img
-            width=${logomarkSvgMeta.width}
-            height=${logomarkSvgMeta.height}
-            style=${{ height: "50%", objectFit: "contain" }}
-            src=${logomarkSvg}
-          />
         </div>
       </div>
     </div>
